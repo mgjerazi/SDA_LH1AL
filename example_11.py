ea75d2e0d4428a957dc5318d3a53454c44355f23
Patryk Walaszkowski <patryk.walaszkowski@jeppesen.com>
2021-09-15 06:30:58 +0200

    move_examples

--- example_11.py
+++
@@ -1,8 +0,0 @@
-sentence = "this is a test for length"
-length = len(sentence)
-print(length == len(sentence))
-
-
-I = "i"
-l = "l"
-
--- example_2.py
+++
@@ -1,9 +0,0 @@
-print(type("Hello Rei"))
-print(type(25))
-print(type(True))
-print(type(False))
-print(type(0.123456789))
-print(type(None))
-
-# Workaround to stop program executing
-input()
