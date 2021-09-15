25dd39d51209d3862cac4120329e7500e97822a1
pwalaszkowski <patrat.github@gmail.com>
2021-09-15 06:20:52 +0200

    Add files via upload

---
+++ example_10.py
@@ -0,0 +1,12 @@
+# Define a variables
+surname = "Walaszkowski"
+print(surname)
+surname = surname.upper()
+print(surname)
+surname = surname.lower()
+print(surname)
+
+seq = [1, 5, 7, 3, 4, 2]
+print(seq)      # before the change
+seq.sort()
+print(seq)      # after the change
---
+++ example_5.py
@@ -0,0 +1,20 @@
+num = 3
+num = num + 4
+print(num)
+num += 4
+print(num)
+num -= 1    # num = num - 1
+print(num)
+num = num * 3   # num = num * 3
+print(num)
+num /= 2    # num = num / 2
+print(num)
+num **= 3   # num = num ** 3    num = num * num * num
+print(num)
+num = num // 2
+print(num)
+
+# print(float(10))
+# print(int(10.12341512))
+
+# print(15 // 7)  # without remainder
